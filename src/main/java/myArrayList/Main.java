6b2b78068092846ff588fe8b04310520676e0184
Anton Mikhailouski <Lobetcki@gmail.com>
2023-02-16 14:34:54 +0300

    My IntegerList 16022023

--- src/main/java/myArrayList/Main.java
+++ src/main/java/myArrayList/Main.java
@@ -5,95 +5,182 @@ import myArrayList.model.StringListImpl;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+
+    private static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public static void sortBubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void sortSelection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+
+    public static void sortInsertion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
     public static void main(String[] args) {
 
-        IntegerListImpl integerListImpl = new IntegerListImpl();
-//        IntegerListImpl integerListImpl = new IntegerListImpl(15);
 
 
-        System.out.println(integerListImpl.add(5));
-        System.out.println(integerListImpl.add(65));
-        System.out.println(integerListImpl.add(84));
-        System.out.println(integerListImpl.add(321));
-        System.out.println(integerListImpl.add(684));
-        System.out.println(integerListImpl.add(65));
-        System.out.println(integerListImpl.add(784));
-        System.out.println(integerListImpl.add(9847));
-        System.out.println(integerListImpl.add(584));
-        System.out.println(integerListImpl.add(9874));
+        Random random = new Random(); // Массивы рандомных чисел
 
-        System.out.println(integerListImpl.add(684));
-        System.out.println(integerListImpl.add(165));
+        int[] randomArray1 = new int[100_000];
 
-        //        System.out.println(integerListImpl.add(null));
-        System.out.println(integerListImpl);
-
-        System.out.println("Кол-во элементов " + integerListImpl.size());
-        System.out.println("______________________________________");
-
-        System.out.println(integerListImpl.add(2,6847684));
-        System.out.println(integerListImpl);
-
-        System.out.println("Кол-во элементов " + integerListImpl.size());
-        System.out.println("______________________________________");
-
-        System.out.println("Замена " + integerListImpl.get(5) + " на " + integerListImpl.set(5, 6544684));
-
-        System.out.println(integerListImpl);
-        System.out.println("Кол-во элементов " + integerListImpl.size());
-        System.out.println("______________________________________");
-
-        System.out.println(integerListImpl.removeIndex(9));
-//        System.out.println(integerListImpl.remove());
-        System.out.println(integerListImpl.remove(321));
-
-        System.out.println(integerListImpl);
-        System.out.println("Кол-во элементов " + integerListImpl.size());
-        System.out.println("______________________________________");
-
-        System.out.println(integerListImpl.contains(784));
-        System.out.println(integerListImpl.contains(4));
-
-        System.out.println("______________________________________");
-
-        System.out.println(integerListImpl.indexOf(784));
-        System.out.println(integerListImpl.indexOf(4));
-
-        System.out.println("______________________________________");
-
-        System.out.println(integerListImpl.lastIndexOf(784));
-        System.out.println(integerListImpl.lastIndexOf(4));
-
-        System.out.println("______________________________________");
-
-        Integer[] integerListImpl2 = integerListImpl.toArray();
-
-        System.out.println(Arrays.toString(integerListImpl2));
-
-        System.out.println("______________________________________");
-
-        IntegerListImpl integerListImpl3 = new IntegerListImpl();
-        System.out.println(integerListImpl3.add(1));
-        System.out.println(integerListImpl3.add(2));
-
-        IntegerListImpl integerListImpl4 = new IntegerListImpl();
-        System.out.println(integerListImpl4.add(1));
-        System.out.println(integerListImpl4.add(2));
+        for (int i = 0; i < randomArray1.length; i++) {
+            randomArray1[i] = random.nextInt();
+        }
 
 
-        System.out.println(integerListImpl3);
-        System.out.println(integerListImpl4);
+        int[] randomArray2 = Arrays.copyOf(randomArray1, randomArray1.length);
 
-        System.out.println("Сравнение " + integerListImpl4.equalsArr(integerListImpl3));
+        int[] randomArray3 = Arrays.copyOf(randomArray1, randomArray1.length);
 
-        System.out.println("______________________________________");
 
-        integerListImpl.clear();
-        System.out.println(integerListImpl);
+        long start1 = System.currentTimeMillis();
+        sortBubble(randomArray1);
+        System.out.println(System.currentTimeMillis() - start1);
 
-        System.out.println(integerListImpl.isEmpty());
+        long start2 = System.currentTimeMillis();
+        sortSelection(randomArray2);
+        System.out.println(System.currentTimeMillis() - start2);
+
+        long start3 = System.currentTimeMillis();
+        sortInsertion(randomArray3);
+        System.out.println(System.currentTimeMillis() - start3);
+
+
+//        IntegerListImpl integerListImpl = new IntegerListImpl();
+        IntegerListImpl integerListImpl = new IntegerListImpl(100_000);
+
+        for (int i = 0; i < 100_000; i++) {
+            integerListImpl.add(random.nextInt());
+        }
+
+        long start4 = System.currentTimeMillis();
+        integerListImpl.contains2(56_658);
+        System.out.println(System.currentTimeMillis() - start4);
+
+
+//        System.out.println(integerListImpl);
+
+
+
+
+
+//        System.out.println(integerListImpl.add(5));
+//        System.out.println(integerListImpl.add(65));
+//        System.out.println(integerListImpl.add(84));
+//        System.out.println(integerListImpl.add(321));
+//        System.out.println(integerListImpl.add(684));
+//        System.out.println(integerListImpl.add(65));
+//        System.out.println(integerListImpl.add(784));
+//        System.out.println(integerListImpl.add(9847));
+//        System.out.println(integerListImpl.add(584));
+//        System.out.println(integerListImpl.add(9874));
+//
+//        System.out.println(integerListImpl.add(684));
+//        System.out.println(integerListImpl.add(165));
+//
+//        //        System.out.println(integerListImpl.add(null));
+//        System.out.println(integerListImpl);
+//
+//        System.out.println("Кол-во элементов " + integerListImpl.size());
+//        System.out.println("______________________________________");
+//
+//        System.out.println(integerListImpl.add(2,6847684));
+//        System.out.println(integerListImpl);
+//
+//        System.out.println("Кол-во элементов " + integerListImpl.size());
+//        System.out.println("______________________________________");
+//
+//        System.out.println("Замена " + integerListImpl.get(5) + " на " + integerListImpl.set(5, 6544684));
+//
+//        System.out.println(integerListImpl);
+//        System.out.println("Кол-во элементов " + integerListImpl.size());
+//        System.out.println("______________________________________");
+//
+//        System.out.println(integerListImpl.removeIndex(9));
+////        System.out.println(integerListImpl.remove());
+//        System.out.println(integerListImpl.remove(321));
+//
+//        System.out.println(integerListImpl);
+//        System.out.println("Кол-во элементов " + integerListImpl.size());
+//        System.out.println("______________________________________");
+//
+//        System.out.println(integerListImpl.contains(784));
+//        System.out.println(integerListImpl.contains(4));
+//
+//        System.out.println("______________________________________");
+//
+//        System.out.println(integerListImpl.indexOf(784));
+//        System.out.println(integerListImpl.indexOf(4));
+//
+//        System.out.println("______________________________________");
+//
+//        System.out.println(integerListImpl.lastIndexOf(784));
+//        System.out.println(integerListImpl.lastIndexOf(4));
+//
+//        System.out.println("______________________________________");
+//
+//        Integer[] integerListImpl2 = integerListImpl.toArray();
+//
+//        System.out.println(Arrays.toString(integerListImpl2));
+//
+//        System.out.println("______________________________________");
+//
+//        IntegerListImpl integerListImpl3 = new IntegerListImpl();
+//        System.out.println(integerListImpl3.add(1));
+//        System.out.println(integerListImpl3.add(2));
+//
+//        IntegerListImpl integerListImpl4 = new IntegerListImpl();
+//        System.out.println(integerListImpl4.add(1));
+//        System.out.println(integerListImpl4.add(2));
+//
+//
+//        System.out.println(integerListImpl3);
+//        System.out.println(integerListImpl4);
+//
+//        System.out.println("Сравнение " + integerListImpl4.equalsArr(integerListImpl3));
+//
+//        System.out.println("______________________________________");
+//
+//        integerListImpl.clear();
+//        System.out.println(integerListImpl);
+//
+//        System.out.println(integerListImpl.isEmpty());
 
 
 
--- src/main/java/myArrayList/model/IntegerListImpl.java
+++ src/main/java/myArrayList/model/IntegerListImpl.java
@@ -19,6 +19,10 @@ public class IntegerListImpl implements IntegerList {
         this.array = new Integer[DEFAULT_CAPACITY];
     }
 
+    public IntegerListImpl(Integer[] array) {
+        this.array = array;
+    }
+
     public IntegerListImpl (int initialCapacity) {
         this.array = new Integer[initialCapacity];
     }
@@ -111,7 +115,39 @@ public class IntegerListImpl implements IntegerList {
         }
         return false;
     }
+                                                        // Сортировка
+    public void sortInsertion() {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] >= temp) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = temp;
+        }
+    }
+                                                    // Бинарный поиск
+    public boolean contains2(int element) {
+        sortInsertion();
+        int min = 0;
+        int max = array.length - 1;
 
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (element == array[mid]) {
+                return true;
+            }
+
+            if (element < array[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
                                                                                         // Поиск элемента.
     // Вернуть индекс элемента или -1 в случае отсутствия.
     @Override
@@ -185,6 +221,8 @@ public class IntegerListImpl implements IntegerList {
         }
         return false;
     }
+
+
 
     @Override
     public String toString() {
